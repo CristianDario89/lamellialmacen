0804f89a9db6f7c84000e9f664ef2e5d61b91dee
CR10crack <ccristiandarioromero@gmail.com>
2020-05-28 12:18:28 -0300

    animatedtext added

--- js/js1.js
+++ js/js1.js
@@ -1,1 +1,6 @@
- 
+function takeInput(){
+         var txt = alert("GRACIAS POR SU COMPRA. LO CONTACTAREMOS EN BREVE.") ;
+         var output = document.getElementById("output");
+         output.innerHTML = txt;
+    
+}  
