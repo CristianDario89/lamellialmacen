29f3885626996026f88966ff197b9db565266746
Cristian <ccristiandarioromero@gmail.com>
2020-07-06 15:30:24 -0300

    nueva orden

--- jsdatos/firebase.js
+++ jsdatos/firebase.js
@@ -1,37 +1,32 @@
 
 firebase.initializeApp({
-   apiKey: "AIzaSyCsWEtRzkkoA-mcGW8oBBTbBPYFn5tU_Ts",
- authDomain: "pedidosonlinemelli.firebaseapp.com",
-      projectId: "pedidosonlinemelli"
+ apiKey: "AIzaSyCU7vTfpu5XrxoZ1UkMa5IV7Rvnu_sE3js",
+  authDomain: "pedidospalomar-a0d1a.firebaseapp.com",
+ projectId: "pedidospalomar-a0d1a"
 });
 var db = firebase.firestore();
    
  
 //Agregar documentos
 function guardar(){
-    var nombre = document.getElementById('name').value;
-    var dire = document.getElementById('dire').value;
+     var dire = document.getElementById('dire').value;
     var email = document.getElementById('email').value;
-    var mensaje = document.getElementById('message').value;
-    var entrega = document.getElementById('entrega').value;
+    
+      var entrega = document.getElementById('entrega').value;
     var pago = document.getElementById('pago').value;
     
-    db.collection("pedidos").add({
+    db.collection("compraonline").add({
          celular: email,
-       nombre: nombre,
-         dire: dire,
+          dire: dire,
           entrega: entrega,
-       pago: pago, 
-         mensaje: mensaje
+       pago: pago 
     })
     .then(function(docRef) {
         console.log("Document written with ID: ", docRef.id);
         prompt("GRACIAS por comprar online, le responderemos para confirmar que su pedido ya ha sido armado para su entrega...");
-        document.getElementById('name').value = '';
-        document.getElementById('dire').value = '';
+          document.getElementById('dire').value = '';
      
-        document.getElementById('email').value = '';
-        document.getElementById('message').value = '';
+        document.getElementById('email').value = ''; 
           document.getElementById('entrega').value = '';
         document.getElementById('pago').value = ''; 
     })
