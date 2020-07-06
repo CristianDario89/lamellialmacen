0c8cb7d971e6c0140d025ec23c42d89ead783b06
Cristian <ccristiandarioromero@gmail.com>
2020-07-06 12:05:07 -0300

    un solo js transfo...

--- datosgalletitas.js
+++ datosgalletitas.js
@@ -1,409 +1,195 @@
 var datosDulces = [ {
-    id:"1",
     marca: "<strong>Don Satur</strong>",
     descripcion: "",
     precio: 40,
-    cantidad:"",
-    seccion:"",
-    proveedor:"",
     img: "images/galle/g2.jpg"
 },
 {
-    id:"2",
     marca: "<strong>Pitusas</strong>",
     descripcion: "Mouse-Limon-chocolate-vainilla-frutilla",
-    cantidad:"",
-    precio: 45,
-    cantidad:"",
-    seccion:"",
-    proveedor:"",
-    img: "images/galle/gad11.jpg"
+     precio: 45,
+   img: "images/galle/gad11.jpg"
     
 }, 
 {
-    id:"3",
     marca: "Mantecadas",
-    descripcion: "",
     precio: 40,
-    cantidad:"",
-    seccion:"",
-    proveedor:"",
     img: "images/galle/mante.jpg"
 },
 {
-    id:"4",
     marca: "Pepitas",
-    descripcion: "",
-    cantidad:"",
-    precio: 40,
-    cantidad:"",
-    seccion:"",
-    proveedor:"",
+     precio: 40,
     img: "images/galle/pepi.jpg" 
 },
 {
-    id:"5",
     marca: "<strong>celosas</strong> bananas con chocolate 230g",
-    descripcion: "",
-    cantidad:"",
     precio: 55,
-    cantidad:"",
-    seccion:"",
-    proveedor:"" ,
     img: "images/galle/gg1.webp"
 },
 {
-    id:"6",
     marca: "<strong>coronitas</strong>",
-    descripcion: "",
-    cantidad:"",
-    precio: 40,
-    cantidad:"",
-    seccion:"",
-    proveedor:"",
+     precio: 40,
     img: "images/galle/gad4.jpg" 
 },
 {
-    id:"7",
     marca: "pepas <strong>trio</strong>",
-    descripcion: "",
-    cantidad:"",
     precio: 45,
-    cantidad:"",
-    seccion:"",
-    proveedor:"",
     img: "images/galle/gad6.jpg" 
 },
 {
-    id:"8",
-    marca: "pepas <strong>pepin</strong>",
+     marca: "pepas <strong>pepin</strong>",
     descripcion: "membrillo-batata",
-    cantidad:"",
     precio: 40,
-    cantidad:"",
-    seccion:"",
-    proveedor:"",
     img: "images/galle/gad5.jpg"
 },
 {
-    id:"9",
     marca: "surtidas <strong>bagley</strong>",
-    descripcion: "",
-    cantidad:"",
     precio: 100,
-    cantidad:"",
-    seccion:"",
-    proveedor:"",
     img: "images/galle/gd1.jpg" 
 }
 ];
 /*-----------------------------*/
 var datosSaladas = [ {
-    id:"1",
     marca: "<strong>Don Satur</strong>",
-    descripcion: "",
     precio: 40,
-    cantidad:"",
-    seccion:"",
-    proveedor:"",
     img: "images/galle/g3.jpg" 
 },
 {
-    id:"2",
     marca: "<strong>media tarde</strong>  x 3",
     descripcion: "$28 c/u",
     precio: 73,
-    cantidad:"",
-    seccion:"",
     img:"images/galle/g4.webp"
 }, 
 {
-    id:"3",
     marca: "<strong>Jorgito</strong> Bizcochitos",
-    descripcion: "",
-    precio: 50,
-    cantidad:"",
-    seccion:"",
+     precio: 50,
     img:"images/galle/gd2.png"
 },
 {
-    id:"4",
-    marca: "<strong>criollitas</strong> x 3",
+     marca: "<strong>criollitas</strong> x 3",
     descripcion: "$35 c/u",
-    cantidad:"",
-    precio: 90,
-    cantidad:"",
-    seccion:"",
+     precio: 90,
     img:"images/galle/g5.webp" 
 },                    
 {
-    id:"",
     marca: "<strong>Jorgito</strong>Bizcochitos ",
-    descripcion: "",
-    cantidad:"",
     precio: 40,
-    cantidad:"",
-    seccion:"",
-    proveedor:"" ,
     img: "images/galle/gad6.jpg" 
 }
 ]  
-
+/*---------------------------------------*/
 var snacks = [ {
-    id:"1",
-    marca: "<strong>lays</strong>",
+     marca: "<strong>lays</strong>",
     descripcion: "doritos / cheetos / 3d",
     precio: 60,
-    cantidad:"",
-    seccion:"",
-    proveedor:"",
     img:"images/galle/snack1.jpg"
 },
 {
-    id:"2",
-    marca: "<strong>saladix</strong>",
-    descripcion: "",
-    cantidad:"",
+     marca: "<strong>saladix</strong>",
     precio: 40,
-    cantidad:"",
-    seccion:"",
     img:"images/galle/g8.jpg" 
 },
 {
-    id:"3",
-    marca: "<strong>Kranchitos</strong> papas",
-    descripcion: "",
-    cantidad:"",
+     marca: "<strong>Kranchitos</strong> papas",
     precio: 55,
-    cantidad:"",
-    seccion:"",
     img:"images/galle/sn2.webp" 
 },
 {
-    id:"4",
     marca: "<strong>twistos</strong> chico",
-    descripcion: "",
-    cantidad:"",
     precio: 55,
-    cantidad:"",
-    seccion:"",
     img:"images/galle/g6.jpg" 
-},
-{
-    id:"5",
-    marca: "<strong>rex</strong>",
-    descripcion: "",
-    cantidad:"",
-    precio: 40,
-    cantidad:"",
-    seccion:"",
-    img:"" 
 }
 ];
-
-/*--------------------------------*/
-var galletitas = [ {
-    id:"",
-    secciones:["budines","facturas","dulces","saladas"],
-    descripcion: "",
-    proveedores:["budines","facturas","dulces","saladas"]   
-} 
-] 
-
-/*--------------------------------*/
-var proveedores = [ {
-    id:"",
-    nombre:"",
-    telefono: "",
-    direccion:"",
-    productos:["budines","facturas","dulces","saladas"]   
-} 
-] 
-
-
-/*----------------------------------------*/
+/*----------------------------------------*//*----------------------------------------*/
+/*----------------------------------------*//*----------------------------------------*/
 var cervezas = [ {
-    id:"1",
-    marca: " <strong>Shneider</strong> lata",
-    descripcion: "",
-    precio: 55,
-    cantidad:"",
-    seccion:"",
-    proveedor:"" ,
+   marca: " <strong>Shneider</strong> lata",
+   precio: 55,
     img: "images/bebi/b10.png" 
 },
 {
-id:"2",
-    marca: " <strong>heineken</strong> lata",
-    descripcion: "",
+   marca: " <strong>heineken</strong> lata",
     precio: 90,
-    cantidad:"",
-    seccion:"",
-    proveedor:"" ,
     img: "images/bebi/cer3.jpg" 
 },               
 {
-    id:"3",
     marca: "<strong>brahama</strong> lata",
-    descripcion: "",
     precio: 70,
-    cantidad:"",
-    seccion:"",
-    proveedor:"" ,
     img: "images/bebi/b1.jpg" 
 },
 {
-    id:"4",
-    marca: "<strong>Quilmes</strong> lata ",
-    descripcion: " ",
-    cantidad:"",
-    precio: 70,
-    cantidad:"",
-    seccion:"",
-    proveedor:"" ,
+     marca: "<strong>Quilmes</strong> lata ",
+     precio: 70,
     img: "images/bebi/b4.jpg"  
 }            
 ];
-
+/*---------------------------------------*/
 var vinos = [ {
-    id:"1",
     marca: "<strong>Michel torino</strong> 720ml",
-    descripcion: "",
     precio: 75,
-    cantidad:"",
-    seccion:"",
     img:"images/bebi/vinos1.jpg"
 },
 {
-    id:"2",
     marca: "<strong>Estancia mendoza</strong> 800ml",
-    descripcion: "",
-    cantidad:"",
-    precio: 120,
-    cantidad:"",
-    seccion:"",
+   precio: 120,
     img:"images/bebi/vinos2.jpg"
 },
 {
-    id:"3",
-    marca: "<strong>Federico de alvear</strong> extra dulce 1L",
-    descripcion: "",
+     marca: "<strong>Federico de alvear</strong> extra dulce 1L",
     precio: 185,
-    cantidad:"",
-    seccion:"",
     img:"images/bebi/vinos3.jpg"
 },
 {
-    id:"4",
     marca: "<strong>Termidor tinto</strong>",
-    descripcion: "",
     precio: 75,
-    cantidad:"",
-    seccion:"",
     img:"images/bebi/vinos3.jpg"
 }
 ];
-
+/*---------------------------------------*/
 var jugos = [ {
-    id:"1",
     marca: "Fresh",
-    descripcion: "",
     precio: 50,
-    cantidad:"",
-    seccion:"",
     img:"images/bebi/b9.webp"
 },
 {
-    id:"2",
-    marca: "Baggio",
-    descripcion: "",
-    cantidad:"",
-    precio: "",
-    cantidad:"",
-    seccion:"",
-    img:"images/bebi/b8.jpg" 
+   marca: "Baggio",
+   precio: "",
+   img:"images/bebi/b8.jpg" 
 },
 {
-    id:"3",
     marca: "tang",
-    descripcion: "",
-    cantidad:"",
     precio: 20,
-    cantidad:"",
-    seccion:"",
     img:"images/bebi/bb4.jpeg" 
 },
 {
-    id:"4",
-    marca: "clight",
-    descripcion: "",
-    cantidad:"",
+     marca: "clight",
     precio: 20,
-    cantidad:"",
-    seccion:"",
     img:"images/bebi/bb1.jpg" 
 },
 {
-    id:"5",
     marca: "<strong> Coca-cola</strong> 2 1/4",
-    descripcion: "",
-    cantidad:"",
     precio: 140,
-    cantidad:"",
-    seccion:"",
     img:"images/bebi/bb6.jpeg" 
 },
 {
-    id:"6",
-    marca: "<strong> Manaos</strong>",
-    descripcion: "",
-    cantidad:"",
+     marca: "<strong> Manaos</strong>",
     precio: 55,
-    cantidad:"",
-    seccion:"",
     img:"images/bebi/gase2.jpg" 
 },
 {
-    id:"7",
-    marca: "<strong>lata coca mini</strong>",
-    descripcion: "",
-    cantidad:"",
+     marca: "<strong>lata coca mini</strong>",
     precio:45,
-    cantidad:"",
-    seccion:"",
     img:"images/bebi/bg1.jpg" 
 },
 {
-    id:"8",
     marca: "<strong> 7up</strong> 2 1/4",
-    descripcion: "",
-    cantidad:"",
     precio: 140,
-    cantidad:"",
-    seccion:"",
     img:"images/bebi/b2.jpg" 
 }
 ];
-
-/*--------------------------------*/
-var todos = [ {
-    id:"",
-    secciones:["cervezas","vinos","jugos","otrasbeb"],
-    descripcion: "",
-    proveedores:["budines","facturas","dulces","saladas"]   
-} 
-] 
-
-/*--------------------------------*/
-var proveedores = [ {
-    id:"",
-    nombre:"",
-    telefono: "",
-    direccion:"",
-    productos:["budines","facturas","dulces","saladas"]   
-} 
-] 
-/*---------------------------------------*/
+ /*---------------------------------------*//*---------------------------------------*/
+ /*---------------------------------------*//*---------------------------------------*/
+/*---------------------------------------*//*---------------------------------------*/
 var over1 = function(atrr,on,tw, thre) {
 	    var contenido2 = document.querySelector(atrr)
 	        contenido2.innerHTML = `
@@ -442,9 +228,6 @@ over1("#ps2", datosSaladas[1].marca,datosSaladas[1].precio,datosSaladas[1].img);
 over1("#ps3", datosSaladas[2].marca,datosSaladas[2].precio,datosSaladas[2].img);
 over1("#ps4", datosSaladas[3].marca,datosSaladas[3].precio,datosSaladas[3].img);
 
-
-
-
 /*------- -*/
 over1("#p10", snacks[1].marca,snacks[1].precio,snacks[1].img);
 over1("#p12", snacks[3].marca,snacks[3].precio,snacks[3].img);  
@@ -453,10 +236,9 @@ over1("#pb6", jugos[5].marca,jugos[5].precio,jugos[5].img);
 over1("#pb8", jugos[7].marca,jugos[7].precio,jugos[7].img);
 
  
-over2("#pbj2", jugos[1].marca,jugos[1].precio,jugos[1].img,"Baggio chico <strong>$ 25</strong>");
-
 over1("#pbj4", jugos[3].marca,jugos[3].precio,jugos[3].img);
-
+over1("#pbj1", jugos[0].marca,jugos[0].precio,jugos[0].img);
+over1("#pbj3", jugos[2].marca,jugos[2].precio,jugos[2].img);
 
 over1("#pb15", vinos[0].marca,vinos[0].precio,vinos[0].img);
 over1("#pb16", vinos[1].marca,vinos[1].precio,vinos[1].img);
@@ -484,8 +266,8 @@ over1("#pb18", vinos[3].marca,vinos[3].precio,vinos[3].img);
 <p href="" data-name="${on}" data-price="${tw}" class="add-to-cart btn btn-primary">AGREGAR</p> 
    `
     }  
-
  
+over2("#pbj2", jugos[1].marca,jugos[1].precio,jugos[1].img,"Baggio chico <strong>$ 25</strong>");
 over2("#pb5", jugos[4].marca,jugos[4].precio,jugos[4].img,"Sprite - fanta");
 over2("#p9", snacks[0].marca,snacks[0].precio,snacks[0].img,"cheetos - 3d - doritos");
 over2("#p11", snacks[2].marca,snacks[2].precio,snacks[2].img,"ketchup/cheedar <strong> $ 60</strong>"); 
@@ -494,19 +276,13 @@ over2("#ps2", datosSaladas[1].marca,datosSaladas[1].precio,datosSaladas[1].img, 
 
 over2("#p2", datosDulces[1].marca,datosDulces[1].precio,datosDulces[1].img,"Mouse - Limon - chocolate - vainilla - frutilla");
 
-
- over2("#pb1", cervezas[0].marca,cervezas[0].precio,cervezas[0].img,"Botella 720ml <strong>$ 110</strong>");
+over2("#pb1", cervezas[0].marca,cervezas[0].precio,cervezas[0].img,"Botella 720ml <strong>$ 110</strong>");
 over2("#pb2", cervezas[1].marca,cervezas[1].precio,cervezas[1].img,"Botella 720ml <strong>$ 140</strong>");
 over2("#pb3", cervezas[2].marca,cervezas[2].precio,cervezas[2].img,"Botella 710ml <strong>$ 110</strong>" );
 over2("#pb4", cervezas[3].marca,cervezas[3].precio,cervezas[3].img, "");
 
-
-over1("#pbj1", jugos[0].marca,jugos[0].precio,jugos[0].img);
-
-
-over1("#pbj3", jugos[2].marca,jugos[2].precio,jugos[2].img);
-
 over2("#pb7", jugos[6].marca,jugos[6].precio,jugos[6].img,"lata 473ml / botella 500ml <strong>$ 60</strong>");
+
 
 // ************************************************
 // Shopping Cart API
